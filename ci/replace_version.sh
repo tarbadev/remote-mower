2109c8c30735b14c569d4a2da55efd06d8078eab
CI System <tarbadev+ci-system@gmail.com>
2020-05-02 16:33:09 -0700

    [finishes #172525245] Add ci/cd configuration

--- ci/replace_version.sh
+++ ci/replace_version.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-export SEMVER_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
+set -e
 
-if [ -z "$SEMVER_LAST_TAG" ]; then
-    >&2 echo "No tags defined"
-    SEMVER_LAST_TAG="1.0.0"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CURRENT_VERSION=$(awk -F': ' '/"version"/{print$2}' package.json | tr -d "\"" | tr -d ",")
+NEW_VERSION=$("$CURRENT_DIR/get_build_version.sh")
+
+>&2 echo "Current version: $CURRENT_VERSION"
+
+if [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then
+  echo "Replacing package.json with version $NEW_VERSION"
+
+  STRING_TO_REPLACE="\"version\": \"$CURRENT_VERSION\""
+  NEW_STRING="\"version\": \"$NEW_VERSION\""
+
+  sed "s/$STRING_TO_REPLACE/$NEW_STRING/g" package.json > new_package.json
+
+  rm package.json
+  mv new_package.json package.json
+else
+  >&2 echo "Not bumping version, keeping version $CURRENT_VERSION"
+  exit 0
 fi
-
-echo $SEMVER_LAST_TAG
 
 exit 0
--- e2e/page-objects/helpers.po.js
+++ e2e/page-objects/helpers.po.js
@@ -1,31 +1,4 @@
-export const waitForTextByCss = (cssSelector, text) => {
-  global.page.waitForFunction(
-    (selector, textToSearch) => {
-      const element = document.querySelector(selector)
-      if (element && element.innerText) {
-        return element.innerText.toLowerCase().includes(textToSearch)
-      }
-    },
-    {},
-    cssSelector.replace(/"/g, '\\"'),
-    text.toLowerCase(),
-  )
-}
-
-export const waitForTextNotEmptyByCss = (cssSelector) => {
-  global.page.waitForFunction(
-    `document.querySelector("${cssSelector.replace(/"/g, '\\"')}").innerText`,
-  )
-}
-
-export const getTextByCssSelector = selector => {
-  return global.page.$eval(
-    selector,
-    element => element.textContent,
-  )
-}
-
-export const waitForElementExist = async (selector) => await global.client.waitForExist(selector)
+export const waitForElementExist = async (selector) => await global.client.waitForExist(selector, { timeout: 2000 })
 
 export const isElementVisible = async (selector) => await global.client.isVisible(selector)
 
