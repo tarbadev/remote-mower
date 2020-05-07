84fe26a2cc151888c7330459b368333b1e829edc
Tarbadev <tarbadev@gmail.com>
2020-05-06 23:30:55 -0700

    [finishes #171991885] Add auto update to application

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,38 @@
-const electron = require('electron')
-const app = electron.app
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 const isDev = require('electron-is-dev')
-const BrowserWindow = electron.BrowserWindow
+const log = require('electron-log')
+const { autoUpdater } = require('electron-updater')
+
+autoUpdater.logger = log
+autoUpdater.logger.transports.file.level = 'info'
+log.info('App starting...')
+
+function logMessage(text) {
+  log.info(text)
+}
+
+autoUpdater.on('checking-for-update', () => {
+  logMessage('Checking for update...')
+})
+autoUpdater.on('update-available', (info) => {
+  logMessage('Update available.')
+})
+autoUpdater.on('update-not-available', (info) => {
+  logMessage('Update not available.')
+})
+autoUpdater.on('error', (err) => {
+  logMessage('Error in auto-updater. ' + err)
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')'
+  logMessage(log_message)
+})
+autoUpdater.on('update-downloaded', (info) => {
+  logMessage('Update downloaded')
+})
 
 let mainWindow
 
@@ -26,7 +56,27 @@ function createWindow() {
   })
 }
 
-app.whenReady().then(createWindow)
+app.on('ready', function () {
+  const menu = Menu.buildFromTemplate([{
+    label: app.name,
+    submenu: [
+      {
+        label: 'About Remote Mower',
+        role: 'about',
+      },
+      {
+        label: 'Quit',
+        role: 'quit',
+      },
+    ],
+  }])
+
+  Menu.setApplicationMenu(menu)
+
+  createWindow()
+
+  autoUpdater.checkForUpdatesAndNotify()
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
